af39e6ae86541a4ace3d42c79c3db17699c8afc0
Santiago2209-03 <202260471@ucc.mx>
2025-12-01 16:57:43 -0600

    ordinario

--- Tareas santi aplicaciones/Proyectos/Android Studio/Mapas/MainActivity.java
+++ Tareas santi aplicaciones/Proyectos/Android Studio/Mapas/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.mapas;
 
 import android.os.Bundle;
-import org.osmdroid.config.Configuration;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.config.Configuration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        Configuration.getInstance().setUserAgentValue(getPackageCodePath());
+        Configuration.getInstance().setUserAgentValue(getPackageName());
         setContentView(R.layout.activity_main);
         map = findViewById(R.id.map);
         map.setMultiTouchControls(true);
@@ -30,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
         map.getController().setZoom(17);
 
         //Marcador
-      Marker marker = new Marker(map);
-      marker.setPosition(startPoint);
-      marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-      marker.setTitle("CDMX");
-      map.getOverlays().add(marker);
+        Marker marker = new Marker(map);
+        marker.setPosition(startPoint);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setTitle("CDMX");
+        map.getOverlays().add(marker);
 
     }
 }
